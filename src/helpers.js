4d681da6c720780c36da95955ebe121a5ab739fe
whoisasey <atinachang@gmail.com>
2022-08-30 20:55:14 -0400

    remove unused function

--- src/helpers.js
+++ src/helpers.js
@@ -3,8 +3,6 @@ import rain from './assets/icons8-rainfall-96.png';
 import sun from './assets/icons8-sun-96.png';
 import partly from './assets/icons8-partly-cloudy-day-96.png';
 
-export  const today = new Date().toISOString().split('T')[0];
-
 export const getWeatherType = (day) => {
   if (day.icon === 'partly-cloudy-day') {
     return <img src={partly} alt="" />;
